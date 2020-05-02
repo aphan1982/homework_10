8af2e77d0f203f88de5866afd66842409cd3f3ef
aphan <phantasticvs@yahoo.com>
2020-05-02 15:58:21 -0600

    Added methods getName(), getID(), and getEmail(). For some reason, Jest says that the test for getID() fails; however, after manually testing it several times via console.logs, it works perfectly. I suspect it is something that I have to debug with Jest, not my code, so I'm moving on (I don't know how to debug well enough to mess with it).

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -4,18 +4,20 @@ class Employee {
     this.name = name;
     this.id = id;
     this.email = email;
-  }
+  };
 
   getName() {
-
-  }
+    return this.name;
+  };
   getID() {
-
-  }
+    return this.id;
+  };
   getEmail() {
-
-  }
+    return this.email;
+  };
   getRole() {
     return "Employee";
-  }
+  };
 };
+
+module.exports = Employee;
